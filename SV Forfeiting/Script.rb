1a5dbf8012acfb6b2bdbd9e132e38e1bb41c01cc
RosieTheHedgie <95107150+AppletunsApples@users.noreply.github.com>
2025-05-23 09:56:20 -0400

    Update Script.rb

--- SV Forfeiting/Script.rb
+++ SV Forfeiting/Script.rb
@@ -1,29 +1,27 @@
 class Battle
   
-  def pbDebugRun
-    return 0 if !$DEBUG || !Input.press?(Input::CTRL)
-    commands = [_INTL("Treat as a win"), _INTL("Treat as a loss"),
-                _INTL("Treat as a draw"), _INTL("Treat as running away/forfeit")]
-    commands.push(_INTL("Treat as a capture")) if wildBattle?
-    commands.push(_INTL("Cancel"))
-    case pbShowCommands(_INTL("Choose the outcome of this battle."), commands)
-    when 0   # Win
-      @decision = 1
-    when 1   # Loss
-      @decision = 2
-    when 2   # Draw
-      @decision = 5
-    when 3   # Run away/forfeit
-          pbDisplayPaused(_INTL("Would you like to give up on this battle and quit now?"))
-          if pbDisplayConfirm(_INTL("Quitting the battle is the same as losing the battle."))
-            @decision = 2   # Treated as a loss
-            return 1
-          end
-    when 4   # Capture
-      return -1 if trainerBattle?
-      @decision = 4
-    else
-      return -1
-    end
-    return 1
+  alias __original_pbRun pbRun
+
+def pbRun(idxBattler, duringBattle = false)
+  battler = @battlers[idxBattler]
+
+  if battler.opposes?
+    return 0 if trainerBattle?
+    @choices[idxBattler][0] = :Run
+    @choices[idxBattler][1] = 0
+    @choices[idxBattler][2] = nil
+    return -1
   end
+
+  if trainerBattle?
+    if pbDisplayConfirm(_INTL("Would you like to forfeit the match and quit now?"))
+      pbSEPlay("Battle flee")
+      pbDisplay(_INTL("{1} forfeited the match!", self.pbPlayer.name))
+      @decision = 3
+      return 1
+    end
+    return 0
+  end
+
+  __original_pbRun(idxBattler, duringBattle)
+end
