46b072aed71eb86496f371dcb76f679500b27e21
RosieTheHedgie <95107150+AppletunsApples@users.noreply.github.com>
2025-08-02 11:42:59 -0400

    removikng ng+ until I commit a bugfix

--- New Game Plus/Settings.rb
+++
@@ -1,6 +0,0 @@
-module NewGamePlusSettings
-  START_LEVEL = 5
-  IV_OVERRIDE = [nil, nil, nil, nil, nil, nil] # HP, Atk, Def, SpE, SpA, SpD
-  FORCE_SHINY = true
-  RESET_PARTY_FIRST_STAGE = true # Reset each party member to their first stage
-end
