5f9ca5ecd07eb2848dfdc31f0fbb8e199435bff4
RosieTheHedgie <95107150+AppletunsApples@users.noreply.github.com>
2025-10-30 16:28:51 -0400

    Updating the script so eggs are pregenerated upon starting the save + renaming Global module

--- Mystery Egg/Script.rb
+++ Mystery Egg/Script.rb
@@ -1,15 +1,15 @@
 #===============================================================================
 # Mystery Egg Plugin by Hedgie
 # One-time-per-save egg that hatches into a random Pokémon with custom egg moves.
+# Egg is kept per save but rerandomizes when starting a new save.
 # Egg hatches after 1 step by default.
 # Requires a new save; Call with MysteryEgg::EggGenerator.give_mystery_egg.
 #===============================================================================
 # Add a new global variable to save the mystery egg species
 class PokemonGlobalMetadata
-  attr_accessor :mysteryEggSpecies
+  attr_accessor :MysteryEgg
 end
 
-# MysteryEgg module
 module MysteryEgg
   # Define possible Pokémon species and their egg moves
   POKEMON_DATA = {
@@ -22,40 +22,32 @@ module MysteryEgg
 
   module EggGenerator
     def self.give_mystery_egg
-      # Select species if not already chosen
-      if !$PokemonGlobal.mysteryEggSpecies
-        $PokemonGlobal.mysteryEggSpecies = POKEMON_DATA.keys.sample
+      if !$PokemonGlobal.MysteryEgg
+        species = POKEMON_DATA.keys.sample
+        egg = MysteryEgg.generate_basic_egg(species)
+        egg.moves.clear
+        POKEMON_DATA[species].each { |move| egg.learn_move(move) }
+        egg.steps_to_hatch = 1
+        $PokemonGlobal.MysteryEgg = egg
       end
-      species = $PokemonGlobal.mysteryEggSpecies
-      egg_moves = POKEMON_DATA[species]
-
-      # Generate egg
-      egg = MysteryEgg.generate_basic_egg(species)
-      if egg
-        egg.moves = []  # Clear default moves
-        egg_moves.each { |move| egg.learn_move(move) } if egg_moves
-        egg.steps_to_hatch = 1  # Hatch after 1 step
-        pbAddPokemonSilent(egg)
-        pbMessage(_INTL("You received a mysterious egg containing {1}!", species.to_s.capitalize))
-      else
-        pbMessage("Failed to create the egg.")
-      end
+      egg = $PokemonGlobal.MysteryEgg
+      pbAddPokemonSilent(egg)
+      pbMessage(_INTL("You received a mysterious egg containing {1}!", 
+        GameData::Species.get(egg.species).name))
     end
   end
 
-def self.generate_basic_egg(species_symbol)
-  species_data = GameData::Species.try_get(species_symbol)
-  return nil if species_data.nil?
-
-  egg = Pokemon.new(species_data.id, Settings::EGG_LEVEL)
-  egg.name           = _INTL("Egg")
-  egg.steps_to_hatch = species_data.hatch_steps
-  egg.obtain_text    = _INTL("Day-Care Couple")
-  egg.happiness      = 120
-  egg.form           = 0 if species_symbol == :SINISTEA
-  # Set regional form
-  new_form = MultipleForms.call("getFormOnEggCreation", egg)
-  egg.form = new_form if new_form
-  return egg
+  def self.generate_basic_egg(species_symbol)
+    species_data = GameData::Species.try_get(species_symbol)
+    return nil if species_data.nil?
+    egg = Pokemon.new(species_data.id, Settings::EGG_LEVEL)
+    egg.name           = _INTL("Egg")
+    egg.steps_to_hatch = species_data.hatch_steps
+    egg.obtain_text    = _INTL("Day-Care Couple")
+    egg.happiness      = 120
+    egg.form           = 1 if species_symbol == :APPLIN
+    new_form = MultipleForms.call("getFormOnEggCreation", egg)
+    egg.form = new_form if new_form
+    return egg
   end
 end
